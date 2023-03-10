051a59da279badbc566ae6b767d1f40b2b692fac
them2dt <mtcreatives@hotmail.com>
2023-03-10 16:14:56 +0100

    add visual effects

--- src/components/MintButton.tsx
+++ src/components/MintButton.tsx
@@ -1,69 +1,89 @@
-import { PublicKey } from '@solana/web3.js';
-import { getMerkleProof, Metaplex, walletAdapterIdentity } from '@metaplex-foundation/js';
-import { useConnection } from '@solana/wallet-adapter-react';
-import { useWallet } from '../hooks/useWallet';
-import { useState } from 'react';
+import { PublicKey } from "@solana/web3.js";
+import {
+  getMerkleProof,
+  Metaplex,
+  walletAdapterIdentity,
+} from "@metaplex-foundation/js";
+import { useConnection } from "@solana/wallet-adapter-react";
+import { useWallet } from "../hooks/useWallet";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 export const MintButton = () => {
-    const [loading, setLoading] = useState(false)
-    const { connection } = useConnection();
-    const wallet = useWallet();
+  const [loading, setLoading] = useState(false);
+  const { connection } = useConnection();
+  const wallet = useWallet();
 
-    const onClick = async () => {
-        // const allowList = await (await fetch("/allowList.json")).json()
+  const onClick = async () => {
+    // const allowList = await (await fetch("/allowList.json")).json()
 
-        if (!wallet.publicKey) {
-            console.log('error', 'Wallet not connected!');
-            return;
-        }
-
-        // if (!allowList.includes(wallet.publicKey.toBase58())) {
-        //     toast.error("You're not on the allowlist");
-        //     return;
-        // }
-
-        setLoading(true)
-
-        const metaplex = Metaplex.make(connection).use(walletAdapterIdentity(wallet))
-        const candyMachineAddress = new PublicKey("8Z1r48UAvmghrYP23hCr18tN7cRhcEEafSAWnnWpms7K");
-        
-        const cm = await metaplex.candyMachines().findByAddress({ address: candyMachineAddress });
-
-        let res;
-            
-        try {
-            // await metaplex.candyMachines().callGuardRoute({
-            //     candyMachine: cm,
-            //     guard: "allowList",
-            //     settings: {
-            //         path: "proof",
-            //         merkleProof: getMerkleProof(allowList, metaplex.identity().publicKey.toBase58())
-            //     }
-            // })
-
-            res = await metaplex.candyMachines().mint({
-                candyMachine: cm,
-                collectionUpdateAuthority: new PublicKey("PeRXuY1P4cnzDZEPH1ancRVSyQMDpnTF27BwmQ1kkWq")
-            })
-        } catch (error) {
-            console.log(error);
-            setLoading(false)
-            return;
-        }
-
-        console.log(res);
-
-        setLoading(false)
+    if (!wallet.publicKey) {
+      console.log("error", "Wallet not connected!");
+      return;
     }
 
-    return (
-      <div className='mint-button'>
-      <button
-          className={"px-8 btn btn-block border-none animate-pulse bg-gradient-to-r from-[#67aafc] to-[#ee982f] hover:from-pink-500 hover:to-yellow-500 " + (loading && " loading")}
-          onClick={() => onClick()}
-      >
-          <span>Mint</span>
-      </button>
-      </div>
+    // if (!allowList.includes(wallet.publicKey.toBase58())) {
+    //     toast.error("You're not on the allowlist");
+    //     return;
+    // }
+
+    setLoading(true);
+
+    const metaplex = Metaplex.make(connection).use(
+      walletAdapterIdentity(wallet)
     );
+    const candyMachineAddress = new PublicKey(
+      "8Z1r48UAvmghrYP23hCr18tN7cRhcEEafSAWnnWpms7K"
+    );
+
+    
+    const cm = await metaplex
+      .candyMachines()
+      .findByAddress({ address: candyMachineAddress });
+
+    let res;
+
+    try {
+      // await metaplex.candyMachines().callGuardRoute({
+      //     candyMachine: cm,
+      //     guard: "allowList",
+      //     settings: {
+      //         path: "proof",
+      //         merkleProof: getMerkleProof(allowList, metaplex.identity().publicKey.toBase58())
+      //     }
+      // })
+
+      res = await metaplex.candyMachines().mint({
+        candyMachine: cm,
+        collectionUpdateAuthority: new PublicKey(
+          "PeRXuY1P4cnzDZEPH1ancRVSyQMDpnTF27BwmQ1kkWq"
+        ),
+      });
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      return;
+    }
+
+    console.log(res);
+
+    setLoading(false);
+  };
+
+  return (
+    <div className="mint-button">
+      <motion.button
+        className={
+          "px-8 btn btn-block border-none animate-pulse bg-gradient-to-r from-[#67aafc] to-[#ee982f] hover:from-pink-500 hover:to-yellow-500 " +
+          (loading && " loading")
+        }
+        onClick={() => onClick()}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.2, delay: 0.6 }}
+      >
+        <span>Mint for 10 SOL</span>
+      </motion.button>
+    </div>
+  );
 };
