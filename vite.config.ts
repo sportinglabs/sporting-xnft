97f58437b9b705b09fb6baf11f61d94b66fa9ca0
Ruben Schaller <rubenusshd@gmail.com>
2023-03-26 22:24:19 +0200

    build error fix

--- vite.config.ts
+++ vite.config.ts
@@ -13,30 +13,33 @@ export default defineConfig(({ command, mode }) => {
     plugins: [react()],
     optimizeDeps: {
       esbuildOptions: {
+        define: {
+          global: "globalThis",
+        },
         plugins: [NodeGlobalsPolyfillPlugin({ process: true, buffer: true })],
       },
     },
     build: {
       commonjsOptions: {
         ignoreTryCatch: (id) => id !== "stream",
-        transformMixedEsModules: true,
       },
-      rollupOptions: {
-        output: {
-          manualChunks: (id) => {
-            const isModule = id.includes("node_modules");
-            if (isModule && id.includes("@solana")) {
-              return "vendor_solana";
-            }
-            if (isModule && id.includes("@project-serum")) {
-              return "vendor_anchor";
-            }
-            if (isModule && id.includes("@tanstack")) {
-              return "vendor_tanstack";
-            }
-          },
-        },
-      },
+      transformMixedEsModules: true,
+      // rollupOptions: {
+      //   output: {
+      //     manualChunks: (id) => {
+      //       const isModule = id.includes("node_modules");
+      //       if (isModule && id.includes("@solana")) {
+      //         return "vendor_solana";
+      //       }
+      //       if (isModule && id.includes("@project-serum")) {
+      //         return "vendor_anchor";
+      //       }
+      //       if (isModule && id.includes("@tanstack")) {
+      //         return "vendor_tanstack";
+      //       }
+      //     },
+      //   },
+      // },
     },
     resolve: {
       alias: [
