751537e357f58710311cf9b66eae7976ae6f99ab
Ruben Schaller <rubenusshd@gmail.com>
2023-03-30 15:05:59 +0200

    integrated sdk

--- src/hooks/useConfig.ts
+++ src/hooks/useConfig.ts
@@ -1,9 +1,13 @@
+import { StakePool } from "@builderz/sporting-f1-sdk";
+import { useConnection } from "@solana/wallet-adapter-react";
 import { useEffect, useState } from "react";
 
 export const useConfig = () => {
   const [config, setConfig] = useState<any>();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  const { connection } = useConnection();
 
   useEffect(() => {
     const fetchConfig = async () => {
@@ -13,6 +17,10 @@ export const useConfig = () => {
         // TODO:
         for (const race of config) {
           console.log(race.poolAddress);
+          const stakePool = await StakePool.fromAccountAddress(
+            connection,
+            race.poolAddress
+          );
         }
 
         setConfig(config);
