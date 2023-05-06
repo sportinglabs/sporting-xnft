9190105c434349fbb42ad1f2aa3205b5ceca5cf3
maruthan4r <mtcreatives@hotmail.com>
2023-05-06 22:05:37 +0200

    apply fonts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,25 +7,24 @@ export default function Home() {
       <div className="top-spacer"></div>
       <div className="home-profile-container">
         <div className="home-profile">
-          <div className="profile-picture"></div>
-          <div className="profile-stat profile-picture">
+          <div className="home-profile-stat home-profile-picture">
             <img src={icon} />
           </div>
-          <div className="profile-stat">
-            <div className="profile-stat-name">points</div>
-            <div className="profile-stat-data">1428</div>
+          <div className="home-profile-stat home-profile-rank">
+            <div className="home-profile-stat-name">Rank</div>
+            <div className="home-profile-stat-data">1428</div>
           </div>
-          <div className="profile-stat">
-            <div className="profile-stat-name">points</div>
-            <div className="profile-stat-data">1428</div>
+          <div className="home-profile-stat home-profile-points">
+            <div className="home-profile-stat-name">points</div>
+            <div className="home-profile-stat-data">1428</div>
           </div>
-          <div className="profile-stat">
-            <div className="profile-stat-name">points</div>
-            <div className="profile-stat-data">1428</div>
+          <div className="home-profile-stat home-profile-last">
+            <div className="home-profile-stat-name">Last</div>
+            <div className="home-profile-stat-data">1428</div>
           </div>
-          <div className="profile-stat">
-            <div className="profile-stat-name">points</div>
-            <div className="profile-stat-data">1428</div>
+          <div className="home-profile-stat home-profile-staked">
+            <div className="home-profile-stat-name">Staked</div>
+            <div className="home-profile-stat-data">1428</div>
           </div>
         </div>
       </div>
